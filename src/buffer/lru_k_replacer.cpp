7f4e606b52c4eb6e44e5e5cf2c62c1323b3c3867
loginfinite <hx2002@mail.ustc.edu.cn>
2024-07-05 13:04:30 +0800

    done lru_k implement

--- src/buffer/lru_k_replacer.cpp
+++ src/buffer/lru_k_replacer.cpp
@@ -15,16 +15,175 @@
 
 namespace bustub {
 
-LRUKReplacer::LRUKReplacer(size_t num_frames, size_t k) : replacer_size_(num_frames), k_(k) {}
+auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool {
+  std::lock_guard<std::mutex>guard(latch_);
+  for (auto it = history_queue_.rbegin();it != history_queue_.rend();it++){
+      if(it->second.Evictable()){
+        *frame_id = it->first;
+        auto pos = history_hash_.find(*frame_id);
+        history_queue_.erase(pos->second);
+        history_hash_.erase(*frame_id);
+        curr_size_--;
+        return true;
+      }
+  }
 
-auto LRUKReplacer::Evict(frame_id_t *frame_id) -> bool { return false; }
+  for (auto it = k_history_queue_.rbegin();it != k_history_queue_.rend();it++){
+      if(it->second.Evictable()){
+        *frame_id = it->first;
+        auto pos = k_history_hash_.find(*frame_id);
+        k_history_queue_.erase(pos->second);
+        k_history_hash_.erase(*frame_id);
+        curr_size_--;
+        return true;
+      }
+  }
 
-void LRUKReplacer::RecordAccess(frame_id_t frame_id, [[maybe_unused]] AccessType access_type) {}
+  return false;
+}
 
-void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable) {}
+void LRUKReplacer::RecordAccess(frame_id_t frame_id, [[maybe_unused]] AccessType access_type) {
+  std::lock_guard<std::mutex>guard(latch_);
+  if(static_cast<size_t>(frame_id) > replacer_size_){
+    BUSTUB_ASSERT(0, "Frame id invalid!");
+  }
+  //If frame is in (< k) history queue
+  auto pos = history_hash_.find(frame_id);
+  if(pos != history_hash_.end()){
+    //val = <frame_id, Node>
+    auto val = *pos->second;
+    BUSTUB_ASSERT(pos->second != history_queue_.end(), "pos in hash doesn't exists in queue");
 
-void LRUKReplacer::Remove(frame_id_t frame_id) {}
+    auto node = val.second;
+    node.Access(access_type);
+    if(node.Kaccess()){
+      //Access more than K times
 
-auto LRUKReplacer::Size() -> size_t { return 0; }
+      //remove from his queue
+      history_queue_.erase(pos->second);
+      history_hash_.erase(frame_id);
+
+
+      //insert to the front of k_his queue
+      k_history_queue_.emplace_front(val);
+      k_history_hash_[frame_id] = k_history_queue_.begin();
+
+    }else{
+      //Access less than K times
+
+      //insert to the front of his queue
+      history_queue_.erase(pos->second);
+      history_queue_.emplace_front(val);
+      history_hash_[frame_id] = history_queue_.begin();
+
+    }
+    return ;
+  }
+
+  pos = k_history_hash_.find(frame_id);
+  if(pos != k_history_hash_.end()){
+    auto val = *pos->second;
+    BUSTUB_ASSERT(pos->second != k_history_queue_.end(), "pos in hash doesn't exists in queue");
+    auto node = val.second;
+    node.Access(access_type);
+    k_history_queue_.erase(pos->second);
+    k_history_queue_.emplace_front(val);
+    k_history_hash_[frame_id] = k_history_queue_.begin();
+    return ;
+  }
+
+
+  //Remove from node_store
+  auto node = LRUKNode(frame_id, k_);
+  node.Access(access_type);
+
+  //insert to the history_queue
+  if(node.Kaccess()){
+    //Access more than K times
+    k_history_queue_.emplace_front(frame_id, node);
+    k_history_hash_[frame_id] = k_history_queue_.begin();
+
+  }else{
+    //Access less than K times
+    history_queue_.emplace_front(frame_id, node);
+    history_hash_[frame_id] = history_queue_.begin();
+  }
+
+}
+
+void LRUKReplacer::SetEvictable(frame_id_t frame_id, bool set_evictable){
+  std::lock_guard<std::mutex>guard(latch_);
+  if(static_cast<size_t>(frame_id) > replacer_size_){
+    BUSTUB_ASSERT(0, "Frame id invalid!");
+  }
+  auto pos = history_hash_.find(frame_id);
+  if(pos != history_hash_.end()){
+    auto &node = pos->second->second;
+    if(node.Evictable() && !set_evictable){
+      curr_size_--;
+    }else if(!node.Evictable() && set_evictable){
+      curr_size_++;
+    }
+    node.SetEvictable(set_evictable);
+    return ;
+  }
+
+  pos = k_history_hash_.find(frame_id);
+  if(pos != k_history_hash_.end()){
+    auto &node = pos->second->second;
+    if(node.Evictable() && !set_evictable){
+      curr_size_--;
+    }else if(!node.Evictable() && set_evictable){
+      curr_size_++;
+    }
+    node.SetEvictable(set_evictable);
+    return ;
+  }
+}
+
+void LRUKReplacer::Remove(frame_id_t frame_id) {
+  std::lock_guard<std::mutex>guard(latch_);
+  auto pos = history_hash_.find(frame_id);
+  if(pos != history_hash_.end()){
+    auto node = pos->second->second;
+    if(node.Evictable()){
+      history_hash_.erase(frame_id);
+      history_queue_.erase(pos->second);
+      curr_size_--;
+      return ;
+    }
+    BUSTUB_ASSERT(1, "Try evict inevitable frame");
+
+  }
+
+  pos = k_history_hash_.find(frame_id);
+  if(pos != k_history_hash_.end()){
+    auto node = pos->second->second;
+    if(node.Evictable()){
+      k_history_hash_.erase(frame_id);
+      k_history_queue_.erase(pos->second);
+      curr_size_--;
+      return ;
+    }
+    BUSTUB_ASSERT(1, "Try evict inevitable frame");
+
+  }
+}
+
+void LRUKNode::SetEvictable(bool evictable) {
+  is_evictable_ = evictable;
+}
+
+void LRUKNode::Access(AccessType accessType) {
+  access_time_ += 1;
+}
+
+auto LRUKNode::Kaccess() -> bool {
+  return access_time_ >= k_;
+}
+
+auto LRUKNode::Evictable() -> bool {
+  return is_evictable_;
+}
 
 }  // namespace bustub
--- src/include/buffer/lru_k_replacer.h
+++ src/include/buffer/lru_k_replacer.h
@@ -26,14 +26,20 @@ namespace bustub {
 enum class AccessType { Unknown = 0, Lookup, Scan, Index };
 
 class LRUKNode {
+ public:
+
+  LRUKNode(frame_id_t frame_id, size_t k_):k_(k_),fid_(frame_id){};
+  void SetEvictable(bool evictable);
+  void Access([[maybe_unused]] AccessType accessType);
+  auto Evictable() -> bool;
+  auto Kaccess() -> bool;
  private:
   /** History of last seen K timestamps of this page. Least recent timestamp stored in front. */
   // Remove maybe_unused if you start using them. Feel free to change the member variables as you want.
-
-  [[maybe_unused]] std::list<size_t> history_;
-  [[maybe_unused]] size_t k_;
-  [[maybe_unused]] frame_id_t fid_;
-  [[maybe_unused]] bool is_evictable_{false};
+  size_t access_time_{};
+  size_t k_{};
+  frame_id_t fid_{};
+  bool is_evictable_{false};
 };
 
 /**
@@ -56,8 +62,7 @@ class LRUKReplacer {
    * @brief a new LRUKReplacer.
    * @param num_frames the maximum number of frames the LRUReplacer will be required to store
    */
-  explicit LRUKReplacer(size_t num_frames, size_t k);
-
+  explicit LRUKReplacer(size_t num_frames, size_t k):replacer_size_(num_frames),k_(k){};
   DISALLOW_COPY_AND_MOVE(LRUKReplacer);
 
   /**
@@ -145,17 +150,22 @@ class LRUKReplacer {
    *
    * @return size_t
    */
-  auto Size() -> size_t;
+  auto Size() -> size_t{
+    return curr_size_;
+  }
 
  private:
   // TODO(student): implement me! You can replace these member variables as you like.
   // Remove maybe_unused if you start using them.
-  [[maybe_unused]] std::unordered_map<frame_id_t, LRUKNode> node_store_;
-  [[maybe_unused]] size_t current_timestamp_{0};
-  [[maybe_unused]] size_t curr_size_{0};
-  [[maybe_unused]] size_t replacer_size_;
-  [[maybe_unused]] size_t k_;
-  [[maybe_unused]] std::mutex latch_;
+  std::list<std::pair<frame_id_t, LRUKNode>> history_queue_;
+  std::unordered_map<frame_id_t, decltype(history_queue_.begin())> history_hash_;
+  std::list<std::pair<frame_id_t, LRUKNode>> k_history_queue_;
+  std::unordered_map<frame_id_t, decltype(history_queue_.begin())> k_history_hash_;
+  size_t current_timestamp_{0};
+  size_t curr_size_{0};
+  size_t replacer_size_;
+  size_t k_;
+  std::mutex latch_;
 };
 
 }  // namespace bustub
