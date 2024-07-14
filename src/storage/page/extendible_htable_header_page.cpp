fdf7d6a146ca614f5f44ce12f60b6e52eb0776db
loginfinite <hx2002@mail.ustc.edu.cn>
2024-07-14 14:50:04 +0800

    finish header page

--- src/storage/page/extendible_htable_header_page.cpp
+++ src/storage/page/extendible_htable_header_page.cpp
@@ -17,17 +17,29 @@
 namespace bustub {
 
 void ExtendibleHTableHeaderPage::Init(uint32_t max_depth) {
-  throw NotImplementedException("ExtendibleHTableHeaderPage is not implemented");
+  BUSTUB_ASSERT(max_depth >= 1 && max_depth < HTABLE_HEADER_MAX_DEPTH, "Invalid max depth");
+  max_depth_ = max_depth;
+  for(auto &directory_page_id: directory_page_ids_){
+    directory_page_id = INVALID_PAGE_ID;
+  }
 }
 
-auto ExtendibleHTableHeaderPage::HashToDirectoryIndex(uint32_t hash) const -> uint32_t { return 0; }
+auto ExtendibleHTableHeaderPage::HashToDirectoryIndex(uint32_t hash) const -> uint32_t {
+  return hash >> (32 - max_depth_);
+}
 
-auto ExtendibleHTableHeaderPage::GetDirectoryPageId(uint32_t directory_idx) const -> uint32_t { return 0; }
+auto ExtendibleHTableHeaderPage::GetDirectoryPageId(uint32_t directory_idx) const -> uint32_t {
+  BUSTUB_ASSERT(directory_idx < HTABLE_HEADER_ARRAY_SIZE, "Invalid directory_idx");
+  return directory_page_ids_[directory_idx];
+}
 
 void ExtendibleHTableHeaderPage::SetDirectoryPageId(uint32_t directory_idx, page_id_t directory_page_id) {
-  throw NotImplementedException("ExtendibleHTableHeaderPage is not implemented");
+  BUSTUB_ASSERT(directory_idx < HTABLE_HEADER_ARRAY_SIZE, "Invalid directory_idx");
+  directory_page_ids_[directory_idx] = directory_page_id;
 }
 
-auto ExtendibleHTableHeaderPage::MaxSize() const -> uint32_t { return 0; }
+auto ExtendibleHTableHeaderPage::MaxSize() const -> uint32_t {
+  return 2 << (max_depth_);
+}
 
 }  // namespace bustub
