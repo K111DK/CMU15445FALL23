290b1f08c7ebb0ce9e5599cc9be25a3dc0a6f77a
loginfinite <hx2002@mail.ustc.edu.cn>
2024-08-17 07:07:42 +0800

    p4: add seqscan mvcc

--- src/execution/seq_scan_executor.cpp
+++ src/execution/seq_scan_executor.cpp
@@ -11,6 +11,10 @@
 //===----------------------------------------------------------------------===//
 
 #include "execution/executors/seq_scan_executor.h"
+#include "execution/execution_common.h"
+#include "concurrency/transaction_manager.h"
+#include "concurrency/transaction.h"
+
 
 namespace bustub {
 
@@ -22,10 +26,38 @@ SeqScanExecutor::SeqScanExecutor(ExecutorContext *exec_ctx, const SeqScanPlanNod
 void SeqScanExecutor::Init() { iterator_ = std::make_shared<TableIterator>(info_->table_->MakeIterator()); }
 
 auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool {
+  auto read_ts = exec_ctx_->GetTransaction()->GetReadTs();
+  auto txn_id_readable = exec_ctx_->GetTransaction()->GetTransactionIdHumanReadable();
+
   while (!iterator_->IsEnd()) {
+
     auto tp = iterator_->GetTuple().second;
     auto tp_meta = iterator_->GetTuple().first;
-    if (!tp_meta.is_deleted_) {
+    auto is_deleted = tp_meta.is_deleted_;
+
+    // Tuple in heap is modified by other txn
+    if( tp_meta.ts_ != txn_id_readable + TXN_START_ID
+        && tp_meta.ts_ != read_ts ){
+
+      // Get all undo logs;
+      auto undo_logs =
+          GetReconstructUndoLogs(exec_ctx_->GetTransactionManager(),
+                                 read_ts,
+                                 tp.GetRid());
+      // Reconstruct tuple
+      auto reconstruct_tp =
+          ReconstructTuple(&plan_->OutputSchema(),
+                           tp,
+                           tp_meta,undo_logs);
+
+      if(reconstruct_tp.has_value()){
+        tp = reconstruct_tp.value();
+      }
+
+      is_deleted = !reconstruct_tp.has_value();
+    }
+
+    if(!is_deleted) {
       if (plan_->filter_predicate_ != nullptr) {
         auto filter_expr = plan_->filter_predicate_;
         auto value = filter_expr->Evaluate(&tp, plan_->OutputSchema());
@@ -35,7 +67,6 @@ auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool {
           ++(*iterator_);
           return true;
         }
-
       } else {
         *tuple = std::move(tp);
         *rid = iterator_->GetRID();
@@ -43,7 +74,9 @@ auto SeqScanExecutor::Next(Tuple *tuple, RID *rid) -> bool {
         return true;
       }
     }
+
     ++(*iterator_);
+
   }
   return false;
 }
--- src/include/execution/execution_common.h
+++ src/include/execution/execution_common.h
@@ -24,6 +24,9 @@ auto ReconstructTuple(const Schema *schema, const Tuple &base_tuple, const Tuple
 void TxnMgrDbg(const std::string &info, TransactionManager *txn_mgr, const TableInfo *table_info,
                TableHeap *table_heap);
 
+
+auto GetReconstructUndoLogs(TransactionManager * transaction_manager,
+                            timestamp_t current_ts, RID rid) -> std::vector<UndoLog>;
 // Add new functions as needed... You are likely need to define some more functions.
 //
 // To give you a sense of what can be shared across executors / transaction manager, here are the
