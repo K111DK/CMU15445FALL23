800d28f3d8b13a2dc363aad336bd8e25f17b08dd
loginfinite <hx2002@mail.ustc.edu.cn>
2024-08-19 05:14:18 +0800

    p4: mvcc: insert

--- src/execution/insert_executor.cpp
+++ src/execution/insert_executor.cpp
@@ -38,26 +38,57 @@ auto InsertExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool {
       return true;
     }
 
-    auto insert_ts = exec_ctx_->GetTransaction()->GetTransactionTempTs();
-    const TupleMeta meta = {insert_ts, false};
-    const auto insert = info_->table_->InsertTuple(meta, *tuple, exec_ctx_->GetLockManager(),
-                                                   exec_ctx_->GetTransaction(), plan_->GetTableOid());
-    if (insert.has_value()) {
-      *rid = insert.value();
-      // Append write set
-      exec_ctx_->GetTransaction()->AppendWriteSet(info_->oid_, insert.value());
-      // Update undo log
-      exec_ctx_->GetTransactionManager()->UpdateUndoLink(insert.value(), std::nullopt);
+    auto txn = exec_ctx_->GetTransaction();
+    auto index_info = exec_ctx_->GetCatalog()->GetTableIndexes(info_->name_);
+    BUSTUB_ASSERT(index_info.size() == 1 && index_info[0]->is_primary_key_,
+                  "In p4, always assume there's only one primary index");
+    const auto &primary_idx = index_info[0];
+    auto primary_hash_table = dynamic_cast<HashTableIndexForTwoIntegerColumn *>(primary_idx->index_.get());
+    auto insert_tuple = tuple->KeyFromTuple(child_executor_->GetOutputSchema(), *primary_hash_table->GetKeySchema(),
+                                            primary_hash_table->GetKeyAttrs());
 
-      auto index_info = exec_ctx_->GetCatalog()->GetTableIndexes(info_->name_);
-      for (const auto &idx : index_info) {
-        auto hash_table = dynamic_cast<HashTableIndexForTwoIntegerColumn *>(idx->index_.get());
-        auto insert_tuple = tuple->KeyFromTuple(child_executor_->GetOutputSchema(), *hash_table->GetKeySchema(),
-                                                hash_table->GetKeyAttrs());
-        hash_table->InsertEntry(insert_tuple, *rid, exec_ctx_->GetTransaction());
-      }
-      total_insert_++;
+    //First, check uniqueness of primary key
+    std::vector<RID> result{};
+    primary_hash_table->ScanKey(insert_tuple, &result, txn);
+
+    //Violate primary key uniqueness. Abort;
+    if(!result.empty()){
+      txn->SetTainted();
+      throw ExecutionException("Abort Txn@" + std::to_string(txn->GetTransactionIdHumanReadable()));
     }
+
+    //Do insert
+    auto insert_ts = exec_ctx_->GetTransaction()->GetTransactionTempTs();
+    TupleMeta meta = {insert_ts, false};
+    RID insert_rid;
+    const auto insert = info_->table_->InsertTuple(meta, *tuple,
+                                                   exec_ctx_->GetLockManager(),
+                                                       exec_ctx_->GetTransaction(),
+                                                   plan_->GetTableOid());
+
+    //In project 4, we always assume insert is successful
+    BUSTUB_ASSERT(insert.has_value(), "Insert fail!");
+    insert_rid = insert.value();
+
+    //Try update primary index
+    bool try_update_primary_index = primary_hash_table->InsertEntry(insert_tuple,
+                                                                    insert_rid,
+                                                                    exec_ctx_->GetTransaction());
+
+    //Fail! Other transaction already update index, mark insert tuple as deleted, then Abort
+    if(!try_update_primary_index){
+      meta = {insert_ts, true};
+      info_->table_->UpdateTupleMeta(meta, insert_rid);
+      txn->SetTainted();
+      throw ExecutionException("Abort Txn@" + std::to_string(txn->GetTransactionIdHumanReadable()));
+    }
+
+    //Success! Append write set
+    exec_ctx_->GetTransaction()->AppendWriteSet(info_->oid_, insert_rid);
+    exec_ctx_->GetTransactionManager()->UpdateUndoLink(insert_rid, std::nullopt);
+
+
+    total_insert_++;
   }
 
   return false;
