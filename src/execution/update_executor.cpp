e37f5ce815d94d0233d3d501e5a78b2eba68522a
loginfinite <hx2002@mail.ustc.edu.cn>
2024-08-19 18:55:11 +0800

    p4: mvcc: fix update

--- src/execution/update_executor.cpp
+++ src/execution/update_executor.cpp
@@ -26,18 +26,18 @@ void UpdateExecutor::Init() { child_executor_->Init(); }
 
 auto UpdateExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool {
   // const TupleMeta delete_meta = {0,true};
-  std::vector<Value> update_tuple{};
+  if(delete_done_){
+    return false;
+  }
 
-  Tuple child_tuple{};
-  while (!delete_done_) {
-    const auto status = child_executor_->Next(&child_tuple, rid);
+  std::vector<std::pair<Tuple, RID>> tuples_to_update{};
+  Tuple temp_tuple{};
+  while(child_executor_->Next(&temp_tuple, rid)){
+    tuples_to_update.emplace_back(temp_tuple, *rid);
+  }
 
-    if (!status) {
-      update_tuple.emplace_back(INTEGER, total_update_);
-      *tuple = Tuple(update_tuple, &GetOutputSchema());
-      delete_done_ = true;
-      return true;
-    }
+  for(auto & [child_tuple, child_rid]:tuples_to_update){
+
     // Compute update tuple
     std::vector<Value> values{};
     values.reserve(GetOutputSchema().GetColumnCount());
@@ -53,7 +53,7 @@ auto UpdateExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool {
     auto read_ts = txn->GetReadTs();
     auto modify_ts = txn->GetTransactionTempTs();
     auto table_info = exec_ctx_->GetCatalog()->GetTable(plan_->table_oid_);
-    auto [meta, tp] = table_info->table_->GetTuple(*rid);
+    auto [meta, tp] = table_info->table_->GetTuple(child_rid);
     bool is_same_transaction = meta.ts_ == modify_ts;
 
     // Abort if a larger ts modify is committed or Any other transaction is modifying
@@ -66,7 +66,7 @@ auto UpdateExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool {
     if (!is_same_transaction) {
       auto [modified_tp, modified_fields] =
           GetTupleModifyFields(&child_executor_->GetOutputSchema(), &child_tuple, &insert_tuple);
-      auto first_undo_version = txn_manager->GetUndoLink(*rid);
+      auto first_undo_version = txn_manager->GetUndoLink(child_rid);
       UndoLog undo_log;
       undo_log.is_deleted_ = meta.is_deleted_;
       undo_log.ts_ = meta.ts_;
@@ -74,14 +74,13 @@ auto UpdateExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool {
       undo_log.tuple_ = modified_tp;
       undo_log.prev_version_ = first_undo_version.has_value() ? first_undo_version.value() : UndoLink();
       auto new_first_undo_version = txn->AppendUndoLog(undo_log);
-      txn_manager->UpdateUndoLink(*rid, new_first_undo_version);
-      txn->AppendWriteSet(table_info->oid_, *rid);
+      txn_manager->UpdateUndoLink(child_rid, new_first_undo_version);
+      txn->AppendWriteSet(table_info->oid_, child_rid);
     } else {
-      auto first_undo_version = txn_manager->GetUndoLink(*rid);
+      auto first_undo_version = txn_manager->GetUndoLink(child_rid);
       if (first_undo_version.has_value() && first_undo_version.value().IsValid()) {
         UndoLog old_undo_log = txn_manager->GetUndoLog(first_undo_version.value());
         std::vector<UndoLog> undo_logs;
-        //GetReconstructUndoLogs(txn_manager, txn->GetReadTs(), *rid, undo_logs);
         UndoLog new_undo_log = old_undo_log;
         auto original_tp = ReconstructTuple(&child_executor_->GetOutputSchema(), tp, meta, {old_undo_log});
         if(original_tp.has_value()){
@@ -93,39 +92,27 @@ auto UpdateExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) -> bool {
         // We only change modified tp
         txn->ModifyUndoLog(first_undo_version->prev_log_idx_, new_undo_log);
       }
-      txn->AppendWriteSet(table_info->oid_, *rid);
+      txn->AppendWriteSet(table_info->oid_, child_rid);
     }
 
     // do modify job
     meta.ts_ = modify_ts;
     meta.is_deleted_ = false;
-    table_info->table_->UpdateTupleInPlace(meta, insert_tuple, *rid);
-
-    //We don't do any modified on (primary)index info
-
-    // delete old index
-//    auto index_info = exec_ctx_->GetCatalog()->GetTableIndexes(table_info_->name_);
-//    for (const auto &idx : index_info) {
-//      auto hash_table = dynamic_cast<HashTableIndexForTwoIntegerColumn *>(idx->index_.get());
-//      auto delete_key = child_tuple.KeyFromTuple(child_executor_->GetOutputSchema(), *hash_table->GetKeySchema(),
-//                                                 hash_table->GetKeyAttrs());
-//      hash_table->DeleteEntry(delete_key, *rid, exec_ctx_->GetTransaction());
-//    }
-
-//    for (const auto &idx : index_info) {
-//      auto hash_table = dynamic_cast<HashTableIndexForTwoIntegerColumn *>(idx->index_.get());
-//      auto insert_key = insert_tuple.KeyFromTuple(child_executor_->GetOutputSchema(), *hash_table->GetKeySchema(),
-//                                                  hash_table->GetKeyAttrs());
-//      bool success = hash_table->InsertEntry(insert_key, *rid, exec_ctx_->GetTransaction());
-//      if (!success) {
-//        BUSTUB_ASSERT(0, "Error: Index insert error");
-//      }
-//    }
-
+    table_info->table_->UpdateTupleInPlace(meta, insert_tuple, child_rid);
     total_update_++;
+    //We don't do any modified on (primary)index info
+    // delete old index
+    //    auto index_info = exec_ctx_->GetCatalog()->GetTableIndexes(table_info_->name_);
+    //    for (const auto &idx : index_info) {
+    //      auto hash_table = dynamic_cast<HashTableIndexForTwoIntegerColumn *>(idx->index_.get());
+    //      auto delete_key = child_tuple.KeyFromTuple(child_executor_->GetOutputSchema(), *hash_table->GetKeySchema(),
+    //                                                 hash_table->GetKeyAttrs());
+    //      hash_table->DeleteEntry(delete_key, *rid, exec_ctx_->GetTransaction());
+    //    }
   }
-
-  return false;
+  *tuple = Tuple({{INTEGER, total_update_}}, &GetOutputSchema());
+  delete_done_ = true;
+  return true;
 }
 
 }  // namespace bustub
